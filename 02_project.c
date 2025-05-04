250431ae561d8e629260724a87865b1a27b71736
Rohit55-cy <kushwahrohit5665@gmail.com>
2025-05-04 17:06:00 +0530

    Create 02_project.c

---
+++ 02_project.c
@@ -0,0 +1,11 @@
+# this is second project
+
+#include <stdio.h>
+
+int main(){
+    int r = 6;
+    int height = 10; 
+    printhf("The area of circle with radius %d is %f", r, 3.14*r*r);
+    printf("The volume of cylinder with radius %d and height %d is %f", r, height, 3.13*r*r*height);  \
+    return 0;
+}
