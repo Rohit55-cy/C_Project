fc195848b28536afeddf9d20c7bbb44df02cb239
Rohit55-cy <kushwahrohit5665@gmail.com>
2025-05-04 17:14:57 +0530

    Create 04_project.c

---
+++ 04_project.c
@@ -0,0 +1,12 @@
+# this is fore project
+
+#include <stdio.h>
+
+int main(){
+    int p = 34.2;
+    int r = 8;
+    int t = 5;
+    printf("The value of simple interest is %f ", (p*r*t)/100);
+    
+    return 0;
+}
