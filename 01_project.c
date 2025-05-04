976996d5a2c06aa18bab857f6ed6edbac543a302
Rohit55-cy <kushwahrohit5665@gmail.com>
2025-05-04 16:59:35 +0530

    Create 01_project.c

---
+++ 01_project.c
@@ -0,0 +1,15 @@
+# this is my first project 
+#include <stdio.h>
+
+int main(){
+    // int lenght - 3;
+    // int breath - 6;
+    printf("Enter length\n");
+    scanf("%d", &length);
+  
+     printf("Enter breath\n");
+    scanf("%d", &breath);
+    printf("The area of this rectangle is %d", length*breath);
+    return 0;
+  
+}
